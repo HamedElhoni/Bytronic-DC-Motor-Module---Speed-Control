6d3ce061ff2c38e59175fda6d6a5e5cfc3b8f51a
Hamedelhoni <hamedelhoni@gmail.com>
2019-05-20 05:00:41 +0200

    Add control through Serial Port
    Add a capability to control of system mode and PID parameter using
    serial port simple commands.

--- src/main.cpp
+++ src/main.cpp
@@ -28,13 +28,15 @@ volatile bool SYNC_enable_flag = false;   //SYNC rising Interrupt Flag
 volatile bool SerialRecivedFlag = false;  //SerialEvent call Flag
 volatile bool ENC_enable_flag = false;    //encrimental encoder Flag
 String SerialReciveData = "";             //Recived Data via SerialEvent
+String Sys_mode = "";                     //to define the close loop or openloop system
 long setpoint = 0;                        //Setpoint for System
 volatile unsigned long counterValue = 0, FeedBack = 0;   //Counter for ENC signal
-volatile unsigned int Op_time =0, sampleTime =0;
+volatile unsigned int Op_time =0, sampleTime =0;  //time in millisecond
 volatile unsigned int FeedBack_RPM;
-bool FeedbackEnable = true;
-int Drive_Signal;
-double Ierror=0.0, Error=0.0,PrevError=0.0,Derror=0.0,Ki=0,Kp=1,Kd=0;               //PID parameter
+bool FeedbackEnable = true;               //to force system between open and close loop operation
+volatile float Drive_Signal;
+volatile float Ierror=0.0, Error=0.0,PrevError=0.0,Derror=0.0,Ki=0,Kp=1,Kd=0;     //PID parameter
+bool startFlag= false;
 //void NewSample(void);
 
 //This ISR of external interrupt attached to pin 2 comes from SYNC 
@@ -42,11 +44,17 @@ void NewSample(){
   sampleTime++;
   if(sampleTime >= 10){
     //Serial.println(Feedback);
+    noInterrupts();
     SYNC_enable_flag = true;
     FeedBack = counterValue;
     FeedBack_RPM =FeedBack*6; //Calculate Feedback as RPM (Round per Minute)
+    Error = setpoint - FeedBack_RPM*FeedbackEnable;
+    Ierror += Error*0.1;
+    Derror = (Error - PrevError)/0.1;
+    Drive_Signal = Error*Kp + Ierror*Ki + Derror*Kd;
     counterValue=0;
     sampleTime = 0;
+    interrupts();
   } 
 }
 
@@ -56,8 +64,7 @@ void CountPulses(){
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200);   //configure bauderate 115200
   pinMode(DIR,OUTPUT);
   attachInterrupt(digitalPinToInterrupt(SYNC),NewSample,RISING);
   attachInterrupt(digitalPinToInterrupt(ENC),CountPulses,FALLING);
@@ -68,9 +75,49 @@ void loop() {
   while (SerialRecivedFlag) {
     SerialRecivedFlag = false;
     //Serial.println(SerialReciveData);
-    setpoint = SerialReciveData.toInt();   // convert recived data to integer number
+    if(SerialReciveData.startsWith("set")){         //set new setpoint 
+      SerialReciveData.remove(0,3);
+      setpoint = SerialReciveData.toInt();
+    } else if(SerialReciveData.startsWith("kp")){ //set new Kp
+      Serial.println(SerialReciveData);
+      SerialReciveData.remove(0,2);
+      Kp=SerialReciveData.toFloat();
+    }else if(SerialReciveData.startsWith("ki")){  //set new Ki
+      SerialReciveData.remove(0,2);
+      Ki=SerialReciveData.toFloat();
+    }else if(SerialReciveData.startsWith("kd")){ //set new Kd
+      SerialReciveData.remove(0,2);
+      Kd=SerialReciveData.toFloat();
+    }else if(SerialReciveData.startsWith("c")){ //Display current configuration
+      Serial.print("Setpoint= ");
+      Serial.print(setpoint);
+      Serial.print("\tKp= ");
+      Serial.print(Kp);
+      Serial.print("\tKi= ");
+      Serial.print(Ki);
+      Serial.print("\tKd= ");
+      Serial.print(Kd);
+      Serial.print("\tMode= ");
+      Serial.println(Sys_mode);
+    }else if(SerialReciveData.startsWith("m")){ // toggle operation mode Close/Open loop
+      if(FeedbackEnable){
+        FeedbackEnable=false;
+        Sys_mode="Open Loop";
+      }else{
+        FeedbackEnable=true;
+        Sys_mode="Close Loop";
+      }
+      Serial.println(FeedbackEnable);
+    }else if(SerialReciveData.startsWith("on")){  // Run the system
+      startFlag = true;
+    }else if(SerialReciveData.startsWith("off")){ // Stop the system
+      startFlag = false;
+    }else{
+      Serial.println("Wrong Input!!");  //Wrong input 
+    }
+    //Serial.println(SerialReciveData);
     //if (setpoint > 255) setpoint = 255;    // to a void over load
-    SerialReciveData = "";
+    SerialReciveData = "";    //Reset to recive new date
     //Serial.println(setpoint);
   }
   if (setpoint >= 0) {
@@ -79,10 +126,6 @@ void loop() {
   else {
     digitalWrite(DIR, HIGH);
   }
-  Error = setpoint - FeedBack_RPM*FeedbackEnable;
-  Ierror += Error*0.1;
-  Derror = (Error - PrevError)/0.1;
-  Drive_Signal = Error*Kp + Ierror*Ki + Derror*Kd;
   //if(Error < 0) Error = 0;  // Error < 0 that mean feedback > setpoint .. Action = motor stop
   if (Drive_Signal > 255){
     Drive_Signal = 255;    // to a void over load
@@ -93,24 +136,35 @@ void loop() {
   analogWrite(PULSE, 255 - Drive_Signal); //PULSE pin -> PIN 5
   while(SYNC_enable_flag){
     SYNC_enable_flag = false;
-    if(FeedBack != 0){
+    if(startFlag){
       //Serial.println(FeedBack);
         //Serial.print("Setpoint= ");
-        Serial.print(millis());
-        Serial.print("\t");
+        Serial.print(Op_time++/10.0);
+        Serial.print(",");
         Serial.print(setpoint);
         //Serial.print("\tFeedBack= ");
         //Serial.print(FeedBack);
-        Serial.print("\t");
+        Serial.print(",");
         Serial.print(FeedBack_RPM);
-        Serial.print("\t");
-        Serial.print(Error);
-        Serial.print("\t");
-        Serial.print(Ierror);
-        Serial.print("\t");
-        Serial.print(Derror);
-        Serial.print("\t");
-        Serial.println(Drive_Signal);
+        if(FeedbackEnable){
+          Serial.print(",");
+          Serial.print(Error);
+          Serial.print(",");
+          Serial.print(Ierror);
+          Serial.print(",");
+          Serial.print(Derror);
+          Serial.print(",");
+          Serial.print(Drive_Signal);
+        }
+        Serial.println("");
+        
+    }else{
+      Drive_Signal = 0;
+      Error=0;
+      Ierror=0;
+      Derror=0;
+      counterValue = 0;
+      Op_time = 0;
     }
   }
 }
